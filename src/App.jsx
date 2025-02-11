7b66370e720caaf84023ea3b7271c9379a5e5a5b
Pranjal <patelpranjal802@gmail.com>
2025-02-11 17:34:58 +0530

    Final Commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-//import Nav from './components/Nav';
 import Nav from '../components/Nav'; 
 import Home from './components/Home';
 import Pastes from './components/Pastes';
